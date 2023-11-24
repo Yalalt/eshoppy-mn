083c3c9b4c74a8f84e1eb7bc2fb2b6628ea9d339
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-25 00:47:48 +0800

    update install toast icons

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    // images: {
-    //     // https://firebasestorage.googleapis.com
-    //     remotePatterns: ['https://firebasestorage.googleapis.com'],
-    // },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            }
+        ]
+    },
 }
 
 module.exports = nextConfig

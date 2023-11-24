42f5ff9066aee0fcf521825a0aff1babd46a859a
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-25 00:42:26 +0800

    add List star rating

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -2,19 +2,24 @@ import React from 'react'
 import Container from '@/app/components/Container';
 import { product } from '@/utils/product';
 import ProductDetails from './ProductDetails';
+import ListRating from './ListRating';
 
 interface IParams {
     productId?: string;  
 }
 
 const Product = ({ params }: { params: IParams }) => {
-    console.log(params, " ===> params");
+    // console.log(params, " ===> params");
     
 
   return (
     <div className='p-8'>
       <Container>
         <ProductDetails product={product} />
+        <div className='flex flex-col mt-20 gap-4'>
+          <div>Add Rating</div>
+          <ListRating product={product} />
+        </div>
       </Container>
     </div>
   )

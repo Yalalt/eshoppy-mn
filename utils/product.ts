fa02b5e368637403a5312523d273e42078b08911
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-25 00:39:29 +0800

    update data

--- utils/product.ts
+++ utils/product.ts
@@ -32,10 +32,10 @@ export const product = {
         id: "6499b4887402b0efd394d8f3",
         userId: "6499b184b0e9a8c8709821d3",
         productId: "648437b38c44d52b9542e340",
-        rating: 4,
+        rating: 2,
         comment:
           "good enough. I like the camera and casing. the delivery was fast too.",
-        createdDate: "2023-06-26T15:53:44.483Z",
+        createdDate: "2023-11-06T15:53:44.483Z",
         user: {
           id: "6499b184b0e9a8c8709821d3",
           name: "Chaoo",
@@ -53,9 +53,9 @@ export const product = {
         id: "6499a110efe4e4de451c7edc",
         userId: "6475af156bad4917456e6e1e",
         productId: "648437b38c44d52b9542e340",
-        rating: 4,
+        rating: 5,
         comment: "I really liked it!!",
-        createdDate: "2023-06-26T14:30:40.998Z",
+        createdDate: "2023-11-06T14:30:40.998Z",
         user: {
           id: "6475af156bad4917456e6e1e",
           name: "Charles",

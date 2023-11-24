33c2e4ae78c9d4bcff0ff27fd4a38f17f2e69308
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-24 19:39:31 +0800

    add Product cart

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -3,6 +3,7 @@ import { formatPrice } from '@/utils/formatPrice';
 import { truncateText } from '@/utils/truncateText';
 import { Rating } from '@mui/material';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 interface ProductCardProps {
@@ -10,11 +11,14 @@ interface ProductCardProps {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+    const router = useRouter();
 
     const productRating = data.reviews.reduce((acc:number, item:any) => item.rating + acc, 0) / data.reviews.length;
 
   return (
-    <div className='col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm'>
+    <div 
+    onClick={() => router.push(`/product/${data.id}`)}
+    className='col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm'>
       <div className='flex flex-col items-center w-full gap-1'>
         <div className='aspect-square overflow-hidden relative w-full'>
           <Image 

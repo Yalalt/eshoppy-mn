4208a68229cdc30fbfcf5229cc246e11cf27da02
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-25 22:20:13 +0800

    Add Cart count number for Navbar

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Container from '../Container';
 import Link from 'next/link';
 import { Redressed } from 'next/font/google';
+import CartCount from './CartCount';
 
 const redressed = Redressed({ subsets: ['latin'], weight: ['400'] });
 
 const NavBar = () => {
+
+
   return (
     <div className='sticky top-0 w-full bg-slate-200 z-30 shadow-sm'>
       <div className='py-4 border-b-[1px]'>
@@ -22,7 +25,7 @@ const NavBar = () => {
             <Link href='/' className={`${redressed.className} font-bold text-2xl`}>eShoppy</Link>
             <div className='hidden md:block'>Search</div>
             <div className='flex items-center gap-8 md:gap-12'>
-              <div>CartCount</div>
+              <CartCount />
               <div>UserMenu</div>
             </div>
           </div>

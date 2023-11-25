69d2c31f3f92bed02fcf073977b10ad5ae7e117b
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-25 22:23:25 +0800

    add amount incre-decre buttons and counter

--- hooks/useCart.tsx
+++ hooks/useCart.tsx
@@ -4,8 +4,13 @@ import { toast } from 'react-hot-toast';
 
 type CartContextType = {
   cartTotalQty: number;
+  cartTotalAmount: number;
   cartProducts: CartProductType[] | null;
   handleAddProductToCart: (product: CartProductType) => void;
+  handleRemoveProductFromCart: (product: CartProductType) => void;
+  handleCartQtyIncrease: (product: CartProductType) => void;
+  handleCartQtyDecrease: (product: CartProductType) => void;
+  handleClearCart: () => void;
 };
 
 export const CartContext = createContext<CartContextType | null>(null);
@@ -16,6 +21,7 @@ interface Props {
 
 export const CartContextProvider = (props: Props) => {
   const [cartTotalQty, setCartTotalQty] = useState(0);
+  const [cartTotalAmount, setCartTotalAmount] = useState(0);
   const [cartProducts, setCartProducts] = useState<CartProductType[] | null>(null);
 
   useEffect(() => {
@@ -24,7 +30,34 @@ export const CartContextProvider = (props: Props) => {
     const cartProducts: CartProductType[] | null = cartItems ? JSON.parse(cartItems) : null;
 
     setCartProducts(cartProducts);
-  }, [])
+  }, []);
+
+  console.log("cartTotalQty ==> ", cartTotalQty);
+  console.log("cartTotalAmount ==> ", cartTotalAmount);
+
+  useEffect(() => {
+    const getTotals = () => {
+      if (cartProducts) {
+        const { total, qty } = cartProducts?.reduce(
+          (acc, item) => {
+            const itemTotal = item.price * item.quantity;
+            acc.total += itemTotal;
+            acc.qty += item.quantity;
+            return acc;
+          },
+          {
+            total: 0,
+            qty: 0,
+          }
+        );
+
+        setCartTotalQty(qty);
+        setCartTotalAmount(total);
+      }
+    };
+
+    getTotals();
+  }, [cartProducts]);
 
   const handleAddProductToCart = useCallback((product: CartProductType) => {
     setCartProducts((prev) => {
@@ -35,19 +68,94 @@ export const CartContextProvider = (props: Props) => {
       } else {
         updateCart = [product];
       }
-      
-      toast.success('Таны сагсанд нэмэгдлээ');
+
+      toast.success('Added to your cart');
       localStorage.setItem('eShoppyCartItems', JSON.stringify(updateCart));
 
       return updateCart;
     });
   }, []);
 
+  const handleRemoveProductFromCart = useCallback(
+    (product: CartProductType) => {
+      if (cartProducts) {
+        const filteredProducts = cartProducts.filter((item) => item.id !== product.id);
+
+        setCartProducts(filteredProducts);
+        toast.success('Removed from your cart');
+        localStorage.setItem('eShoppyCartItems', JSON.stringify(filteredProducts));
+      }
+    },
+    [cartProducts]
+  );
+
+  const handleCartQtyIncrease = useCallback(
+    (product: CartProductType) => {
+      let updatedCart;
+
+      if (product.quantity === 99) {
+        return toast.error('You can have a maximum of 99 items in your cart');
+      }
+
+      if (cartProducts) {
+        updatedCart = [...cartProducts];
+        const existingIndex = updatedCart.findIndex((item) => item.id === product.id);
+
+        if (existingIndex > -1) {
+          updatedCart[existingIndex].quantity += 1;
+        }
+
+        setCartProducts(updatedCart);
+        localStorage.setItem('eShoppyCartItems', JSON.stringify(updatedCart));
+        toast.success('Your cart has been added');
+      }
+    },
+    [cartProducts]
+  );
+
+  const handleCartQtyDecrease = useCallback(
+    (product: CartProductType) => {
+      let updatedCart;
+
+      if (product.quantity === 1) {
+        return toast.error('You can have at least 1 item in your cart');
+      }
+
+      if (cartProducts) {
+        updatedCart = [...cartProducts];
+        const existingIndex = updatedCart.findIndex((item) => item.id === product.id);
+
+        if (existingIndex > -1) {
+          updatedCart[existingIndex].quantity -= 1;
+        }
+
+        setCartProducts(updatedCart);
+        localStorage.setItem('eShoppyCartItems', JSON.stringify(updatedCart));
+        toast.success('Removed from your cart');
+      }
+    },
+    [cartProducts]
+  );
+
+  const handleClearCart = useCallback(() => {
+    setCartProducts(null);
+    setCartTotalQty(0);
+    setCartTotalAmount(0);
+    localStorage.removeItem('eShoppyCartItems');
+    toast.success('Your cart has been cleared');
+  }, []);
+
   const value = {
     cartTotalQty,
+    cartTotalAmount,
     cartProducts,
     handleAddProductToCart,
+    handleRemoveProductFromCart,
+    handleCartQtyIncrease,
+    handleCartQtyDecrease,
+    handleClearCart,
   };
+
   return <CartContext.Provider value={value} {...props} />;
 };
 

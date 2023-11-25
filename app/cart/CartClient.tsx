e041607382987965caa4189345c58d993d50b19c
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-25 22:21:30 +0800

    Add Cart Items for Cart page and handle clear

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -5,9 +5,11 @@ import React from 'react';
 import { MdArrowBack } from 'react-icons/md';
 import Heading from '../components/Heading';
 import Button from '../components/Button';
+import ItemContent from './ItemContent';
+import { formatPrice } from '@/utils/formatPrice';
 
 const CartClient = () => {
-  const { cartProducts } = useCart();
+  const { cartProducts, handleClearCart, cartTotalAmount } = useCart();
 
   if (!cartProducts || cartProducts.length === 0) {
     return (
@@ -32,20 +34,22 @@ const CartClient = () => {
         <div className='justify-self-center'>QUANTITY</div>
         <div className='justify-self-end'>TOTAL</div>
       </div>
+
       <div>
         {cartProducts &&
-          cartProducts.map((product) => {
-            return <div key={product.id}>{product.name}</div>;
+          cartProducts.map((item) => {
+            return <ItemContent key={item.id} item={item} />
           })}
       </div>
+
       <div className='border-t-[1.5px] border-slate-200 py-4 flex justify-between items-start'>
         <div className='w-[90px]'>
-          <Button label='Clear Cart' onClick={() => {}} small outline />
+          <Button label='Clear Cart' onClick={() => {handleClearCart()}} small outline />
         </div>
         <div className='text-sm flex flex-col gap-1 items-start'>
           <div className='flex justify-between w-full text-base font-semibold'>
             <span>Subtotal</span>
-            <span>${90.0}</span>
+            <span>{formatPrice(cartTotalAmount)}</span>
           </div>
           <p className='text-slate-500'>Taxes and shipping calculate at checkout</p>
           <Button label='Checkout' onClick={() => {}} />

83f403980a1e7dd09898489cbb03ed22fa39c0fa
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-26 14:35:06 +0800

    create register form form wrapper

---
+++ app/register/RegisterForm.tsx
@@ -0,0 +1,61 @@
+'use client';
+import React from 'react';
+import Heading from '../components/Heading';
+import Input from '../components/inputs/Input';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Button from '../components/Button';
+import Link from 'next/link';
+import { AiOutlineGoogle } from 'react-icons/ai';
+
+const RegisterForm = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    setIsLoading(true);
+    console.log(data);
+    // setIsLoading(false);
+  };
+
+  return (
+    <>
+      <Heading title='Sign up for eShoppy' />
+
+      <Button outline label='Sign up with Google' icon={AiOutlineGoogle} disabled={isLoading} 
+      onClick={() => {}} />
+      <hr className='bg-slate-300 w-full h-px' />
+      <Input id='name' label='Name' disabled={isLoading} register={register} errors={errors} required />
+      <Input id='email' label='Email' disabled={isLoading} register={register} errors={errors} required />
+      <Input
+        id='password'
+        label='Password'
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+        type='password'
+      />
+
+      <Button label={isLoading ? 'Loading...' : 'Sign up'} disabled={isLoading} onClick={handleSubmit(onSubmit)} />
+
+      <p className='text-sm'>
+        Already have an account?{' '}
+        <Link href='/login' className='underline'>
+          Log In
+        </Link>
+      </p>
+    </>
+  );
+};
+
+export default RegisterForm;
---
+++ app/register/page.tsx
@@ -0,0 +1,16 @@
+import { Container } from '@mui/material';
+import React from 'react';
+import FormWrap from '../components/FormWrap';
+import RegisterForm from './RegisterForm';
+
+const Register = () => {
+  return (
+    <Container>
+      <FormWrap>
+        <RegisterForm />
+      </FormWrap>
+    </Container>
+  );
+};
+
+export default Register;

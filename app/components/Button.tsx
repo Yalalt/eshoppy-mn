15d4ce05cec11578022aa771566030b9d906d4bf
Yalalt Gandush <yalaltgandush@gmail.com>
2023-11-26 14:25:39 +0800

    fix error not load icon

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -42,7 +42,7 @@ const Button: React.FC<ButtonProps> = ({
     ${small ? "py-1 px-2 border-[1px]" : "py-3 px-4 border-2"}
     ${custom ? custom : ""}
     `}>
-        {/* {icon && <Icon size={24} />} */}
+        {Icon && <Icon size={24} />}
         {label}
     </button>
   )

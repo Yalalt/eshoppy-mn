db74f2378a35f687e4ba0a5905138215361c9239
Yalalt Gandush <yalaltgandush@gmail.com>
2023-12-01 22:39:37 +0800

    checkbox input comp

---
+++ app/components/inputs/CustomCheckBox.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { FieldValues, UseFormRegister } from "react-hook-form";
+
+interface CustomeCheckBoxProps {
+    id: string;
+    label: string;
+    disabled?: boolean;
+    register: UseFormRegister<FieldValues>;
+}
+
+const CustomCheckBox: React.FC<CustomeCheckBoxProps> = ({
+    id,
+    label,
+    disabled,
+    register,
+}) => {
+  return (
+    <div className='w-full flex flex-row gap-2 items-center'>
+      <input
+        id={id}
+        type='checkbox'
+        disabled={disabled}
+        {...register(id)}
+        placeholder=""
+        className="cursor-pointer"
+      />
+      <label htmlFor={id} className='font-medium cursor-pointer'>
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default CustomCheckBox;

7694ddbc884776ee2c7ff89ccbd9476184c32616
Yalalt Gandush <yalaltgandush@gmail.com>
2023-12-01 22:36:05 +0800

    add route update product

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -3,14 +3,15 @@ import { NextResponse } from 'next/server';
 import { getCurrentUser } from '@/actions/getCurrentUser';
 
 export async function POST(request: Request) {
-    const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser();
 
-    if(!currentUser || currentUser.role !== 'ADMIN') {
-        return NextResponse.error();
-    }
+  if (!currentUser || currentUser.role !== 'ADMIN') {
+    return NextResponse.error();
+  }
   const body = await request.json();
   const { name, description, price, brand, category, inStock, images } = body;
 
+  console.log('BE::: Body ==> ', body);
 
   const newProduct = await prisma.product.create({
     data: {
@@ -20,9 +21,27 @@ export async function POST(request: Request) {
       category,
       inStock,
       images,
-        price: parseFloat(price),
+      price: parseFloat(price),
     },
   });
 
   return NextResponse.json(newProduct);
+}
+
+export async function PUT(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== 'ADMIN') {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const { id, inStock } = body;
+
+  const product = await prisma.product.update({
+    where: { id: id },
+    data: { inStock: inStock },
+  });
+
+  return NextResponse.json(product);
 }

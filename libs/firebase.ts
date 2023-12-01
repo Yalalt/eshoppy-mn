67314dcca0c5ec686e9546ad08006bbb08107d21
Yalalt Gandush <yalaltgandush@gmail.com>
2023-12-01 22:35:05 +0800

    update settings firebase

--- libs/firebase.ts
+++ libs/firebase.ts
@@ -1,23 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { get } from "http";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: "AIzaSyClTBV46uyDv6gfOAcwQm1w16pSvAI_IOI",
+  authDomain: "eshoppy-a1c97.firebaseapp.com",
   projectId: "eshoppy-a1c97",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_API_ID,
+  storageBucket: "eshoppy-a1c97.appspot.com",
+  messagingSenderId: "519765731200",
+  appId: "1:519765731200:web:2083cc378c0441ec1a8365",
   measurementId: "G-NCM5JDQ9QT"
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
 
 export default firebaseApp;

25587867f13fa415ac65b2db3591056160866a07
Yalalt Gandush <yalaltgandush@gmail.com>
2023-12-01 22:42:47 +0800

    manage order component table pend disp delivered

--- app/admin/manage-orders/page.tsx
+++ app/admin/manage-orders/page.tsx
@@ -1,8 +1,27 @@
+import Container from '@/app/components/Container';
 import React from 'react'
+import ManageOrdersClient from './ManageOrdersClient';
+import { getCurrentUser } from '@/actions/getCurrentUser';
+import NullData from '@/app/components/NullData';
+import getOrders from '@/actions/getOrders';
 
-const ManageOrders = () => {
+
+
+const ManageOrders = async () => {
+  const currentUser = await getCurrentUser();
+  const orders = await getOrders();
+
+  if(!currentUser || currentUser.role !== 'ADMIN') {
+    return <NullData title='Oops! Access Denied' />
+  }
+
+
   return (
-    <div>ManageOrders</div>
+    <div>
+      <Container>
+        <ManageOrdersClient orders={orders} />
+      </Container>
+    </div>
   )
 }
 

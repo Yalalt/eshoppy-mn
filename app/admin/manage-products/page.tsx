6dd2680476afba59c78021e6fc44d5ca3c49204f
Yalalt Gandush <yalaltgandush@gmail.com>
2023-12-01 22:36:54 +0800

    add manage product interface

--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,8 +1,25 @@
+import Container from '@/app/components/Container';
 import React from 'react'
+import ManageProductsClient from './ManageProductsClient';
+import getProducts from '@/actions/getProducts';
+import { getCurrentUser } from '@/actions/getCurrentUser';
+import NullData from '@/app/components/NullData';
 
-const ManageProducts = () => {
+const ManageProducts = async () => {
+
+  const products = await getProducts({category: null});
+  const currentUser = await getCurrentUser();
+
+  if(!currentUser || currentUser.role !== 'ADMIN') {
+    return <NullData title='Oops! Access denied' />
+  }
+
   return (
-    <div>ManageProducts</div>
+    <div className='pt-8'>
+        <Container>
+          <ManageProductsClient products={products} />
+        </Container>
+    </div>
   )
 }
 

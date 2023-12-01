356d67aba82c29291806fee7b05a75fbfb195d6d
Yalalt Gandush <yalaltgandush@gmail.com>
2023-12-01 22:37:27 +0800

    add Get orders action

---
+++ actions/getProducts.ts
@@ -0,0 +1,55 @@
+import prisma from '@/libs/prismadb';
+
+export interface IProductParams {
+  category?: string | null;
+  searchTerms?: string | null;
+}
+
+export default async function getProducts(params: IProductParams) {
+  try {
+    const { category, searchTerms } = params;
+    let searchString = searchTerms;
+
+    if (!searchTerms) {
+      searchString = '';
+    }
+
+    let query: any = {};
+
+    if (category) {
+      query.category = category;
+    }
+
+    const products = await prisma.product.findMany({
+      where: {
+        ...query,
+        OR: [
+          {
+            name: {
+              contains: searchString,
+              mode: 'insensitive',
+            },
+            description: {
+              contains: searchString,
+              mode: 'insensitive',
+            },
+          },
+        ],
+      },
+      include: {
+        reviews: {
+          include: {
+            user: true,
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        },
+      },
+    });
+
+    return products;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}

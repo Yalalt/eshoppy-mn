4fc4e8d06d07dabd74faa6df1f44820a873f99a5
Yalalt Gandush <yalaltgandush@gmail.com>
2023-12-02 21:18:39 +0800

    get Products from Mongodb

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -3,14 +3,19 @@ import Container from '@/app/components/Container';
 import ProductDetails from './ProductDetails';
 import ListRating from './ListRating';
 import { products } from '@/utils/products';
+import getProductById from '@/actions/getProductById';
+import NullData from '@/app/components/NullData';
 
 interface IParams {
     productId?: string;  
 }
 
-const Product = ({ params }: { params: IParams }) => {
-    // console.log(params, " ===> params");
-  const product = products.find((item) => item.id === params.productId);
+const Product = async ({ params }: { params: IParams }) => {
+  const product = await getProductById(params);
+
+  if(!product) {
+    return <NullData title='Oops! No products found. Product id does not exist' />
+  }
 
   return (
     <div className='p-8'>

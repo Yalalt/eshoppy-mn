676f547553adab31ec6ddf1a8c147b7635868ad0
Yalalt Gandush <yalaltgandush@gmail.com>
2024-01-15 22:02:58 +0800

    product route some edit

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -34,7 +34,9 @@ export async function POST(request: Request) {
 export async function PUT(request: Request) {
   const currentUser = await getCurrentUser();
 
-  if (!currentUser || currentUser.role !== 'ADMIN') {
+  if(!currentUser) return NextResponse.error();
+
+  if (currentUser.role !== 'ADMIN') {
     return NextResponse.error();
   }
 

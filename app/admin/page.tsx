f0a177f9b4d85bd951ec0aed1d524cb6abab8095
Yalalt Gandush <yalaltgandush@gmail.com>
2024-01-18 16:38:31 +0800

    add admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Container from '../components/Container';
+import Summary from './Summary';
+import getProducts from '@/actions/getProducts';
+import getOrders from '@/actions/getOrders';
+import getUsers from '@/actions/getUsers';
+import getGraphData from '@/actions/getGraphData';
+import BarGraph from './BarGraph';
 
-const Admin = () => {
+const Admin = async () => {
+  const products = await getProducts({ category: null });
+  const orders = await getOrders();
+  const users = await getUsers();
+  const graphData = await getGraphData();
+
   return (
-    <div>
-        <h1>Admin</h1>
+    <div className='pt-8'>
+      <Container>
+        <Summary products={products} orders={orders} users={users} />
+        <div className='mt-4 mx-auto max-w-[1150px]'>
+          <BarGraph data={graphData} />
+        </div>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
 export default Admin;
